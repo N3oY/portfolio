934279ffb029edc5aa0df91f24c0cedb6fc6f027
N3oY <juarezkevin8@gmail.com>
2025-02-13 10:22:43 -0600

    fix v1

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,8 @@ import express from 'express';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { getContext } from '@netlify/angular-runtime/context'; // <- Elimina esta línea si no es necesaria
+import { CommonEngine } from '@angular/ssr/node'
+import { render } from '@netlify/angular-runtime/common-engine'
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
@@ -65,3 +67,17 @@ if (isMainModule(import.meta.url)) {
  * The request handler used by the Angular CLI (dev-server and during build).
  */
 export const reqHandler = createNodeRequestHandler(app);
+
+​
+const commonEngine = new CommonEngine()
+​
+export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
+  // Example API endpoints can be defined here.
+  // Uncomment and define endpoints as necessary.
+  // const pathname = new URL(request.url).pathname;
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
+​
+  return await render(commonEngine)
+}
